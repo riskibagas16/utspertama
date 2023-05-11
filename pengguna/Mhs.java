1b95ff7c86cfa05d98c27680446749b757cb1415
riskibagas16 <riskibagas31@gmail.com>
2023-05-11 17:46:19 +0700

    UTS

--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,39 @@
 package pengguna;
 
-class Mhs {
-    Mhs () {
+public class Mhs extends User{
+    private String Nama;
+    private String Nim;
+    private int Smt;
 
+
+    public Mhs(String username, String password){
+        super("ini_username", "ini_password");
     }
+
+public void setNama(String nama){
+    this.Nama = nama;
 }
+
+public String getNama(){
+    return this.Nama;
+}
+
+public void setNim(String nim){
+    this.Nim = nim;
+}
+
+public String getNim(){
+    return this.Nim;
+}
+
+public void setSmt(int smt){
+    this.Smt = smt;
+}
+
+public int getSmt(){
+    return this.Smt;
+}
+}
+
+
+
--- pengguna/User.java
+++ pengguna/User.java
@@ -1,5 +1,29 @@
 package pengguna;
+public class User{
+    private String username;
+    private String password;
 
-public class User {
+   public User(String username, String password){
+    this.username = username;
+    this.password = password;
+   }
+
+   public void setUsername(String username){
+    this.username = username;
+   }
+
+   public String getUsername(){
+    return this.username = username;
+   }
+
+   public void setPassword(String password){
+    this.password = password;
+   }
+
+   public String getPassword(){
+    return this.password = password;
+   }
 
 }
+
+
